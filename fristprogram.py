76e447d7ccbfced0e90d7d19d7d85334ba907117
Shilpi Tamrakar <shilpitamrakar06@gmail.com>
2026-01-28 22:49:13 +0530

    update

--- fristprogram.py
+++ fristprogram.py
@@ -1,1 +1,17 @@
 print("hello world")
+print("shilpi tamrakar")
+print("cybrom")
+print("shilpi is my name")
+print("my age is 21")
+print(21)
+print(21+23)
+
+name = "shilpi"
+age = 21
+price = 25.99
+print(name)
+print(age)
+print(price)
+print(type(name))
+print(type(age))
+print(type(price))
