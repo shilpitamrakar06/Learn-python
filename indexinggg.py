81c34821e0f46c9c96a5626d2a25309e87e2c77b
Shilpi Tamrakar <shilpitamrakar06@gmail.com>
2026-01-29 19:21:23 +0530

    update

--- indexinggg.py
+++ indexinggg.py
@@ -5,3 +5,24 @@ print(s.index('s'))
 i = "my name is shilpi"
 print(i.index("is"))
 
+#q.1
+a= [5,10,15,20]
+print(a[2])
+
+
+#q.2
+name = "data"
+print(name[-1])
+
+#q.3
+b=[1,2,3,4,5]
+print(b[0]+b[-1])
+
+#q.4
+x = "python"
+print(x[0:3])
+
+#q.5
+a = [10,20,30]
+a[1] = 99
+print(a) 
